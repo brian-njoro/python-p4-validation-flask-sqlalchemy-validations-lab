e7cd245549ac09857cc998906a0cbdeac0a94bea
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-09-23 17:28:34 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -12,6 +12,21 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_phone_number(self,key,name):
+        if not name:
+            raise ValueError("please enter your name")
+        return name
+    
+    @validates('/phone_number')
+    def validate_phone_number(self,key,phone_number):
+        #removing non-numerical characters 
+        new_number = ''.join(filter(str.isdigit,phone_number))
+        if len(new_number) != 10:
+            raise ValueError("Phone number must consist of only 10 digits")
+        return phone_number
+    
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +41,29 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('title')
+    def validate_title(self,key,title):
+        if not title:
+            raise ValueError("Every post must have a title!")
+        return title
+    
+    @validates('summary')
+    def validate_summary(self,key,summary):
+        if summary and len(summary) > 250:
+            raise ValueError("post summaru cannot esxeed 250 characters!")
+        
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return content
+    
+    @validates('title')
+    def validate_clickbait(self, key, title):
+        bait_words = ["Won't Believe", "Secret", "Guess"]
+        if not any(keyword in title for keyword in bait_words):
+            raise ValueError("Post title must contain at least one clickbait keyword.")
 
 
     def __repr__(self):
